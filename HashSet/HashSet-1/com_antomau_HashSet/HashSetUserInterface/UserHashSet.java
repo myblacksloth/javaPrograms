6a6afd424b7fc448fa545c0dfb5ab69d8690e55d
Antonio Maulucci <antonio.maulucci@libero.it>
2017-01-25 12:44:12 +0100

    Add files via upload

--- HashSet/HashSet-1/com_antomau_HashSet/HashSetUserInterface/UserHashSet.java
+++ HashSet/HashSet-1/com_antomau_HashSet/HashSetUserInterface/UserHashSet.java
@@ -144,6 +144,126 @@ public class UserHashSet {
 		
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		class searchButtonListenerClass implements ActionListener
+		{
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				contentPanel.removeAll();
+				
+				JPanel mainInternalPanel = new JPanel();
+				mainInternalPanel.setLayout(new GridLayout(2,1));
+				
+				
+				
+				JPanel searchPanel = new JPanel();
+				searchPanel.setLayout(new GridLayout(1,3));
+				
+				JPanel dataPanel = new JPanel();
+				dataPanel.setLayout(new GridLayout(3,2));
+				
+				JLabel l1 = new JLabel("Author:");
+				JLabel l2 = new JLabel ("Title:");
+				JLabel l3 = new JLabel("Year:");
+				JTextField jtf1 = new JTextField();
+				JTextField jtf2 = new JTextField();
+				JTextField jtf3 = new JTextField();
+				
+				dataPanel.add(l1);
+				dataPanel.add(l2);
+				dataPanel.add(l3);
+				dataPanel.add(jtf1);
+				dataPanel.add(jtf2);
+				dataPanel.add(jtf3);
+				
+				
+				JPanel typePanel = new JPanel(); //this panel will be user to choose what data find
+				typePanel.setLayout(new GridLayout(4,1));
+				
+				JLabel typeLabel = new JLabel("What data do you want to find?");
+				ButtonGroup typeGroup = new ButtonGroup();
+				JRadioButton authorButton = new JRadioButton("Author");
+				JRadioButton titleButton = new JRadioButton("Title");
+				JRadioButton yearButton = new JRadioButton("Year");
+					
+				typePanel.add(typeLabel); //1,1
+				typePanel.add(authorButton); //2,1
+				typePanel.add(titleButton); //3,1
+				typePanel.add(yearButton); //4,1
+				
+				
+				JButton button = new JButton("Find");
+				
+				
+				searchPanel.add(dataPanel); //1,1
+				searchPanel.add(typePanel); //1,2
+				searchPanel.add(button); //1,3
+				
+				
+				
+				mainInternalPanel.add(searchPanel); //1,1
+				
+				contentPanel.add(mainInternalPanel);
+				
+				class buttonListenerClass implements ActionListener
+				{
+					@Override
+					public void actionPerformed(ActionEvent e)
+					{
+						JPanel resultsPanel = new JPanel();
+						
+						JLabel resultsLabel = new JLabel();
+						/*
+						if (authorButton.isSelected()) resultsLabel.setText(mySet.findAuthor(jtf1.getText()));
+						else if (titleButton.isSelected()) resultsLabel.setText(mySet.findTitle(jtf1.getText()));
+						else if (yearButton.isSelected()) resultsLabel.setText(mySet.findYear(Integer.parseInt(jtf1.getText())));
+						*/	
+						resultsPanel.add(resultsLabel);
+						
+						mainInternalPanel.add(resultsPanel); //2,1
+						f.pack();
+					} //end of internal actionPerformed
+				} //end of buttonListenerClass
+				
+				button.addActionListener(new buttonListenerClass());
+				
+				f.pack();
+			} //end of actionPerformed
+		} //end of exitButtonListenerClass
+		
+		ActionListener searchButtonListener = new searchButtonListenerClass();
+		searchButton.addActionListener(searchButtonListener);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	} //end of main
 	
 	
