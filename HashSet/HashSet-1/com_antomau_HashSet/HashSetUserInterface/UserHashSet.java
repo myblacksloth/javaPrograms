40b6024c4461bff72de9caf2a80f4243db326065
Antonio Maulucci <antonio.maulucci@libero.it>
2017-01-25 12:16:20 +0100

    Add files via upload

--- HashSet/HashSet-1/com_antomau_HashSet/HashSetUserInterface/UserHashSet.java
+++ HashSet/HashSet-1/com_antomau_HashSet/HashSetUserInterface/UserHashSet.java
@@ -1,12 +1,20 @@
 package com_antomau_HashSet.HashSetUserInterface;
 
+import com_antomau_HashSet.Book.Book;
+import com_antomau_HashSet.HashSet.HashSet;
+
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.util.Scanner;
 import java.io.File;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.FileNotFoundException;
 
 public class UserHashSet {
+	
+	private static HashSet mySet = new HashSet(13);
 	
 	private static Scanner in;
 	
@@ -14,6 +22,9 @@ public class UserHashSet {
 	
 	public static void main(String[] args)
 	{
+		
+		JFileChooser fc = new JFileChooser();
+		
 		JFrame f = new JFrame("Antonio Maulucci's HashSet");
 		f.setSize(300, 300);
 		f.setLayout(new BorderLayout());
@@ -23,7 +34,7 @@ public class UserHashSet {
 		
 		
 		JPanel consolePanel = new JPanel();
-		consolePanel.setLayout(new GridLayout(5,1));
+		consolePanel.setLayout(new GridLayout(1,5));
 		
 		JButton openButton = new JButton("Open file");
 		JButton printButton = new JButton("Print all data");
@@ -43,6 +54,96 @@ public class UserHashSet {
 		f.add(contentPanel, BorderLayout.CENTER);
 		
 		f.pack();
+		
+		
+		
+		
+		class exitButtonListenerClass implements ActionListener
+		{
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				System.exit(0);
+			} //end of actionPerformed
+		} //end of exitButtonListenerClass
+		
+		ActionListener exitButtonListener = new exitButtonListenerClass();
+		exitButton.addActionListener(exitButtonListener);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		class openButtonListenerClass implements ActionListener
+		{
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				
+				fc.setDialogTitle("Choose a file to open");
+				fc.showOpenDialog(f);
+				
+				try {
+					file = fc.getSelectedFile();
+					in = new Scanner(file);
+				} catch (FileNotFoundException exc) {
+					exc.printStackTrace();
+					return;
+				}
+				
+				Book newBook = null;
+				
+				while (in.hasNext())
+				{
+					newBook = new Book(in.next(), in.next(), in.nextInt());
+				} //end of while
+				
+				mySet.add(newBook);
+				
+			} //end of actionPerformed
+		} //end of exitButtonListenerClass
+
+		ActionListener openButtonListener = new openButtonListenerClass();
+		openButton.addActionListener(openButtonListener);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		class printButtonListenerClass implements ActionListener
+		{
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				
+				contentPanel.removeAll();
+				
+				JLabel content = new JLabel(mySet.toLabelString());
+				
+				contentPanel.add(content);
+				
+				f.pack();
+				
+			} //end of actionPerformed
+		} //end of exitButtonListenerClass
+		
+		ActionListener printButtonListener = new printButtonListenerClass();
+		printButton.addActionListener(printButtonListener);
+		
+		
+		
+		
 	} //end of main
 	
 	
