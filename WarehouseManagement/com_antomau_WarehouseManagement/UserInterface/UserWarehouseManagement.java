cb18ff298e04f19573e8f97e7ae8cd0777b111d1
Antonio Maulucci <antonio.maulucci@libero.it>
2017-01-29 19:06:17 +0100

    Add files via upload

---
+++ WarehouseManagement/com_antomau_WarehouseManagement/UserInterface/UserWarehouseManagement.java
@@ -0,0 +1,38 @@
+package com_antomau_WarehouseManagement.UserInterface;
+
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.util.Scanner;
+
+
+
+/**
+ * Created by Antonio Maulucci http://www.antomau.com on 29/01/17.
+ */
+public class UserWarehouseManagement {
+
+
+    private static Scanner in;
+
+
+    public static void main(String[] args)
+    {
+
+        JFrame f = new JFrame("Antonio Maulucci's Warehouse Manager");
+        f.setLayout(new BorderLayout());
+        f.setSize(300, 300);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+
+
+
+
+
+        f.pack();
+
+    } //end of main
+
+
+
+
+}
